fcba2d7ff756d9e7d2dadf0d79ec44b6f60894f2
fhb <francisco.borge@docu.ch>
2023-11-03 21:00:12 +0100

    8.63_one-to-many_relation

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\User;
+use App\Entity\Comment;
+use App\Entity\MicroPost;
 use App\Entity\UserProfile;
+use App\Repository\CommentRepository;
+use App\Repository\MicroPostRepository;
 use App\Repository\UserProfileRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HelloController extends AbstractController
 {
@@ -18,8 +23,32 @@ class HelloController extends AbstractController
     ];
     
     #[Route('/', name: 'app_index')]
-    public function index(UserProfileRepository $profiles): Response
+    public function index(MicroPostRepository $posts, CommentRepository $comments): Response
     {
+        // Option 1 without CommentRepository plus cascading in MicroPost relationship
+        // $post = new MicroPost();
+        // $post->setTitle('Hello');
+        // $post->setText('Hello');
+        // $post->setCreated(new DateTime());
+
+        //Option 2
+        //This get the post and make an edition without addComment() and add()
+        $post = $posts->find(15);
+        //$post->getComments()->count(); //This show the content of comment array
+
+        $comment = $post->getComments()[0];
+        $post->removeComment($comment); //This remove the older record
+        $posts->add($post, true); //For this to work, then post needs to be saved
+
+        //dd($post);
+
+        // $comment = new Comment();
+        // $comment->setText('Hello');
+        // $comment->setPost($post);
+        //$post->addComment($comment);
+        //$posts->add($post, true); //This save the post
+        //$comments->add($comment, true); //This function needs to be defined in CommentRepository
+        
         // $user = new User();
         // $user->setEmail('email@email.com');
         // $user->setPassword('12345678');
