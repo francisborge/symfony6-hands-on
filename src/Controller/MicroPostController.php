38b68edb7895cf0952afce2c676a46ab4d8bfb21
fhb <francisco.borge@docu.ch>
2023-11-06 21:24:09 +0100

    9.65_adding_comments

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use DateTime;
 use App\Entity\MicroPost;
+use App\Form\CommentType;
 use App\Form\MicroPostType;
+use App\Repository\CommentRepository;
 use App\Repository\MicroPostRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -84,5 +87,35 @@ class MicroPostController extends AbstractController
                 'form' => $form
             ]
         );
-    }    
+    }
+
+    #[Route('/micro-post/{post}/comment', name: 'app_micro_post_comment')]
+    public function addComment(MicroPost $post, Request $request, CommentRepository $comments): Response
+    {
+        $form = $this->createForm(CommentType::class, new Comment());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $comment->setPost($post);
+            $comments->add($comment, true);
+
+            #dd($post);
+            // Add a flash
+            $this->addFlash('success', 'Your comment has been updated.');
+
+            return $this->redirectToRoute(
+                'app_micro_post_show',
+                ['post' => $post->getId()]
+            ); //you can use an specific path with redirect(/micro-post/1/comments)
+            // Redirect
+        }
+
+        return $this->renderForm('micro_post/comment.html.twig', 
+            [
+                'form' => $form,
+                'post' => $post
+            ]
+        );        
+    }
 }
