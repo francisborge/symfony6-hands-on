2744478122d3e5a29b3cc71b4f6f8e949df468af
fhb <francisco.borge@docu.ch>
2024-07-03 21:21:08 +0200

    13.99_user_posts_optimizing_query

--- src/Repository/MicroPostRepository.php
+++ src/Repository/MicroPostRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -46,6 +47,23 @@ class MicroPostRepository extends ServiceEntityRepository
             withComments: true
         )->getQuery()->getResult();
         
+    }
+
+    public function findAllByAuthor(
+        int | User $author
+    ): array
+    {
+        return $this->findAllQuery(
+            withComments: true,
+            withLikes: true,
+            withAuthors: true,
+            withProfiles: true
+        )->where('p.author = :author')
+        ->setParameter(
+            'author',
+            $author instanceof User ? $author->getId() : $author
+        )->getQuery()
+        ->getResult();
     }
 
     private function findAllQuery(
